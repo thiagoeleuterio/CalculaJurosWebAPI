a76741be2eb11b9b4b12bc28dcf5536a5c5e10ae
Thiago Chagas <thiago.chagas@paradigmabs.com.br>
2020-09-11 13:45:13 -0300

    Add project files.

---
+++ WebApplicationAPI/Controllers/TaxasController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplicationAPI.Models;
+
+namespace WebApplicationAPI.Controllers
+{
+    public class TaxasController : ApiController
+    {
+        /// <summary>
+        /// Calcular o juros de um determinado valor.
+        /// https://localhost:xxxx/api/Taxas
+        /// 
+        /// JSON:
+        /// {
+        ///     "Juros": 0.5,
+        ///     "Valor": 10000
+        /// }
+        /// </summary>
+        /// <param name="valores">Class Valores</param>
+        /// <returns>Retorna o valor acrescido dos juros</returns>
+        public HttpResponseMessage Post([FromBody]Valores valores)
+            {
+                if (valores == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                decimal Juros = valores.Juros / 100 * valores.Valor;
+
+                decimal Resultado = Math.Round(valores.Valor + Juros, 2, MidpointRounding.AwayFromZero);
+
+                var response = Request.CreateResponse(HttpStatusCode.OK, Resultado);
+
+                return response;
+            }
+        }
+}
---
+++ WebApplicationAPI/Models/Valores.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplicationAPI.Models
+{
+    public class Valores
+    {
+        public decimal Juros { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
